25a9e04b4558777a8c80383170d7e6125594ca8e
Iryna Zaporozhets <iryna.zaporozhets@fu-berlin.de>
2023-03-16 13:57:42 +0100

    Separate end2end distance KL div calculation

--- proteka/metrics/calculator.py
+++ proteka/metrics/calculator.py
@@ -4,6 +4,7 @@ from abc import ABCMeta, abstractmethod
 
 from .featurizer import Featurizer
 from ..datadset import Ensemble
+from .divergence import kl_divergence
 
 class IMetrics(metaclass=ABCMeta):
     """Abstract class defining interface for metrics calculators
@@ -70,12 +71,40 @@ class StructuralIntegrityMetrics(IMetrics):
         return {"max z-score": np.max(z), "rms z-score": np.sqrt(np.mean(z ** 2))}
     
 class EnsembleQualityMetrics(IMetrics):
-    """Metrics to compare a target ensemble to the reference enseble
+    """Metrics to compare a target ensemble to the reference ensemble
     """
 
-    def compute(self, ensemble: Ensemble, reference: Ensemble):
+    def compute(self, target: Ensemble, reference: Ensemble):
         """
-        Compute the metrics of interest for 
+        Compute the metrics that compare the target ensemble to the reference 
         """
+        self.results.update(end2end_distance_kl_div(target,reference))
+        return
+        
+        
     
+    
+    @staticmethod    
+    def end2end_distance_kl_div(target: Ensemble, reference: Ensemble) -> dict:
+        """Computes maximum and rms z-score for d_ca_ca bonds over ensemble.
+            Z-score is defined as (d_ca_ca - mean(d_ca_ca)) / std(d_ca_ca)
+            d_ca_ca and std(d_ca_ca) are parametrized based on analysis of the proteka
+            and pdb databases. Beware, that this metric will give high deviations for
+            cis-proline peptide bonds
+        """
+        
+        feat_reference = Featurizer(reference)
+        feat_target = Featurizer(target)
+        
+        # Compute end-to-end distances distributions
+        # TO DO: 
+        d_e2e_reference = feat_reference.get_e2e_distances().in_units_of("nm")
+        d_e2e_target = feat_target.get_e2e_distances().in_units_of("nm")
+        
+        # Histogram of the distances. Will use 100 bins and bin edges extracted from 
+        # the reference ensemble        
+        hist_reference, bin_edges = np.histogram(d_e2e_reference, bins=100, weights=reference.weights)
+        hist_target, _ = np.histogram(d_e2e_target, bins=bin_edges, weights=target.weights)
+        kl = kl_divergence(hist_reference, hist_target)
+        return {"d end2end, KL divergence": kl}
         
