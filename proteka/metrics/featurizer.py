ca5ad75d4f6b63a53449127dddd8197ae0ef2a86
Iryna Zaporozhets <iryna.zaporozhets@fu-berlin.de>
2023-03-20 13:57:58 +0100

    Store features as part of  an ensemble

--- proteka/metrics/featurizer.py
+++ proteka/metrics/featurizer.py
@@ -23,7 +23,7 @@ class Featurizer:
             len(ca_atoms) == self.ensemble.top.n_residues
         ), "Number of CA atoms does not match the number of residues"
 
-    def get_ca_bonds(self) -> Quantity:
+    def add_ca_bonds(self) -> Quantity:
         """
         Returns a Quantity object that contains length of pseudobonds
         between consecutive CA atoms.
@@ -40,11 +40,12 @@ class Featurizer:
             [ca_atoms[i], ca_atoms[i + 1]] for i in range(len(ca_atoms) - 1)
         ]
         ca_bonds = md.compute_distances(trajectory, ca_pairs, periodic=False)
-        return Quantity(
-            ca_bonds, "nanometers", metadata={"feature": "CA-CA pseudobonds"}
-        )
+        quantity = Quantity(ca_bonds, "nanometers", metadata={"feature": "ca_ca_pseudobonds"})
+        self.ensemble.set_quantity("ca_ca_pseudobonds", quantity)
+        return 
+        
 
-    def get_ca_distances(self, offset=1) -> Quantity:
+    def add_ca_distances(self, offset=1) -> Quantity:
         """Get distances between CA atoms.
 
         Parameters:
@@ -64,13 +65,14 @@ class Featurizer:
         ca_distances = md.compute_distances(
             trajectory, ca_pairs, periodic=False
         )
-        return Quantity(
+        quantity = Quantity(
             ca_distances,
             "nanometers",
-            metadata={"feature": "CA-CA distances", "offset": offset},
+            metadata={"feature": "ca_ca_distances", "offset": offset},
         )
+        self.ensemble.set_quantity("ca_ca_distances", quantity)
 
-    def get_ca_angles(self) -> Quantity:
+    def add_ca_angles(self) -> Quantity:
         """Get angles between consecutive CA atoms"""
         trajectory = self.ensemble.get_all_in_one_mdtraj()
         ca_atoms = trajectory.top.select("name CA")
@@ -84,17 +86,19 @@ class Featurizer:
         ca_angles = md.compute_distances(
             trajectory, ca_triplets, periodic=False
         )
-        return Quantity(
+        quantity = Quantity(
             ca_angles,
             "radians",
-            metadata={"feature": "consecutive CA-CA-CA angles"},
+            metadata={"feature": "consecutive_ca_ca_ca_angles"},
         )
+        self.ensemble.set_quantity("consecutive_ca_ca_ca_angles", quantity)
+        return
 
-    def get_ca_bond_vectors(self) -> Quantity:
+    def add_ca_bond_vectors(self) -> Quantity:
         """Get vectors between consecutive CA atoms"""
         raise NotImplementedError
 
-    def get_ca_dihedrals(self) -> Quantity:
+    def add_ca_dihedrals(self) -> Quantity:
         """Get dihedral angles between consecutive CA atoms"""
         trajectory = self.ensemble.get_all_in_one_mdtraj()
         ca_atoms = trajectory.top.select("name CA")
@@ -109,27 +113,29 @@ class Featurizer:
         ca_dihedrals = md.compute_dihedrals(
             trajectory, ca_quadruplets, periodic=False
         )
-        return Quantity(
+        quantity = Quantity(
             ca_dihedrals,
             "radians",
-            metadata={"feature": "consecutive CA-CA-CA_CA dihedrals"},
+            metadata={"feature": "consecutive_ca_ca_ca_ca_dihedrals"},
         )
+        self.ensemble.set_quantity("consecutive_ca_ca_ca_ca_dihedrals", quantity)
+        return
 
-    def get_backbone_torsions(self) -> Quantity:
+    def add_backbone_torsions(self) -> Quantity:
         """Get protein backbone torsions"""
         raise NotImplementedError
 
-    def get_rmsd(self, reference=None, frame=0, atom_indices=None) -> Quantity:
+    def add_rmsd(self, reference=None, frame=0, atom_indices=None) -> Quantity:
         """Get RMSD of a subset of atoms.
         Wrapper of mdtraj.rmsd
         """
         raise NotImplementedError
 
-    def get_rg(self) -> Quantity:
+    def add_rg(self) -> Quantity:
         """Get radius of gyration for each structure in an ensemble"""
         raise NotImplementedError
 
-    def get_end2end_distance(self) -> Quantity:
+    def add_end2end_distance(self) -> Quantity:
         """Get distance between CA atoms of the first and last residue in the protein
         for each structure in the ensemble
         """
@@ -140,8 +146,12 @@ class Featurizer:
         # Get the pair of the first and last CA atoms
         ca_pair = [[ca_atoms[0], ca_atoms[-1]]]
         distance = md.compute_distances(trajectory, ca_pair, periodic=False)
-        return Quantity(
+        quantity = Quantity(
             distance,
             "nanometers",
-            metadata={"feature": "CA-CA end-to-end distance"},
+            metadata={"feature": "ca_ca_end2end_distance"},
         )
+        self.ensemble.set_quantity("ca_ca_end2end_distance", quantity)
+        return
+    
+    
