db9585ce8af3d93fa0361820a2f22107c67b5d30
Iryna Zaporozhets <iryna.zaporozhets@fu-berlin.de>
2023-03-31 15:23:32 +0200

    Adapt to new Ensemble API

--- proteka/metrics/featurizer_test.py
+++ proteka/metrics/featurizer_test.py
@@ -1,19 +1,19 @@
-
 import numpy as np
-import mdtraj as md
 
-from proteka.dataset import Emsemble
+from proteka.dataset import Ensemble, Quantity
 from proteka.metrics import Featurizer
 from proteka.metrics.utils import generate_grid_polymer
 
 
 def test_get_ca_bonds():
     grid_size = 0.4
-    traj = _generate_grid_polymer(n_frames=10, n_atoms=5, grid_size=grid_size)
+    traj = generate_grid_polymer(n_frames=10, n_atoms=5, grid_size=grid_size)
     ens = Ensemble("CAgrid", traj.top, Quantity(traj.xyz, "nm"))
     featurizer = Featurizer(ens)
-    ca_bonds = featurizer.get_ca_bonds()
-    assert np.all(ca_bonds.in_unit_of('nm')) == grid_size
-    
+    featurizer.add_ca_bonds()
+    print(ens.ca_bonds)
+    assert np.all(np.isclose(ens.ca_bonds, grid_size))
+
+
 if __name__ == "__main__":
     test_get_ca_bonds()
