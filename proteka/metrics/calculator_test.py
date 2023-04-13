47fd8b615237128b1b6fe10f9e0d9bbf82dd3029
Iryna Zaporozhets <iryna.zaporozhets@fu-berlin.de>
2023-04-13 13:43:13 +0200

    Add test for ca clashes

--- proteka/metrics/calculator_test.py
+++ proteka/metrics/calculator_test.py
@@ -1,2 +1,24 @@
 """Test calculator
 """
+import numpy as np
+import mdtraj as md
+import pytest
+
+from proteka.dataset import Ensemble
+from proteka.quantity import Quantity
+from proteka.metrics import Featurizer
+from proteka.metrics import StructuralIntegrityMetrics
+from proteka.metrics.utils import get_6_bead_frame
+
+
+@pytest.fixture
+def single_frame():
+    traj = get_6_bead_frame()
+    ensemble = Ensemble("6bead", traj.top, Quantity(traj.xyz, "nm"))
+    return ensemble
+
+def test_ca_clashes(single_frame):
+    clashes = StructuralIntegrityMetrics.ca_clashes(single_frame)
+    print(clashes)
+    assert clashes['ca_clashes'] == 1
+    
