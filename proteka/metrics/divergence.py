19fe4dc4e3f6537b2a8130bfe3f641719369292d
nec4 <nec4@rice.edu>
2023-05-05 00:07:19 +0200

    vector mse

--- proteka/metrics/divergence.py
+++ proteka/metrics/divergence.py
@@ -115,8 +115,40 @@ def mse(target: np.ndarray, reference: np.ndarray) -> float:
     assert (
         target.shape == reference.shape
     ), f"Dimension mismatch: target: {target.shape} reference: {reference.shape}"
+    target_normalized = target / np.sum(target)
+    reference_normalized = reference / np.sum(reference)
 
     return np.average((target - reference) ** 2)
+
+
+def vector_mse(target: np.ndarray, reference: np.ndarray) -> float:
+    r"""
+     Compute Vector Mean Squared Error between specified data sets.
+
+     .. math :: MSE = \frac{1}{N} \sum_{i=1}^N (p_i - q_i)^2
+
+     Here p corresponds to the reference (True) distribution, q corresponds to the target distribution.
+
+     Parameters
+     -----------
+
+     target, reference : np.ndarray
+        Target and reference probability distributions (histograms).
+        Should have the same shape
+
+     Returns : float
+     ------
+    Mean Squared Error of the target from the reference
+
+    """
+    assert (
+        target.shape == reference.shape
+    ), f"Dimension mismatch: target: {target.shape} reference: {reference.shape}"
+
+    target_normalized = target / np.sum(target, axis=0)
+    reference_normalized = reference / np.sum(reference, axis=0)
+
+    return np.average((target - reference) ** 2, axis=0)
 
 
 def vector_kl_divergence(
